a563f9320c4c1ba1e97e1479bbd74ef7c27a01b4
okumu87 <okumu793@gmail.com>
2025-05-03 14:11:47 +0200

    Color FLIPPER APP

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 const colors = ["green", "red", "rgba(133,122,200)", "f15025"];
 
 const btn = document.getElementById("btn");
-const color = document.querySelector("color");
+const color = document.querySelector(".color");
 
-btn.addEventListener("click", function () {});
+btn.addEventListener("click", function () {
+  //get random number
 
-//12.48
+  const randomNumber = getRandomNumber();
+
+  document.body.style.backgroundColor = colors[randomNumber];
+  color.textContent = colors[randomNumber];
+});
+
+function getRandomNumber() {
+  return Math.floor(Math.random() * 3);
+}
